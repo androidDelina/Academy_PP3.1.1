7e4da24cdb69d6412b40b71bc27b76707a1cdc92
Delya <delina.cat@gmail.com>
2024-10-10 23:18:52 +0300

    Remove comments

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,12 +1,8 @@
 package web.service;
 
 import web.model.Car;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public interface CarService {
-
-//    public List<Car> getAll();
     public List<Car> getNCars(int n);
 }
